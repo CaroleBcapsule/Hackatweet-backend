57cf0588e1f84b00a52155008d1a685e2d4ba40e
Carole Bellet <belletcarole@yahoo.fr>
2023-04-28 10:39:13 +0200

    back

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+var app = express();
 const cors = require("cors");
 app.use(cors());
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var tweetsRouter = require("./routes/tweets");
-var app = express();
+
 
 app.use(logger("dev"));
 app.use(express.json());
